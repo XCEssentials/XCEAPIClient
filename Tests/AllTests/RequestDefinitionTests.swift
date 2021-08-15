792c15977407cb3ece56e5db55cdadadd4f18f40
Maxim Khatskevich <maxim@khatskevi.ch>
2021-08-15 19:05:04 +0200

    [ADD] unit tests to expose incorrect handling of optional parameters in request definition

--- Tests/AllTests/RequestDefinitionTests.swift
+++ Tests/AllTests/RequestDefinitionTests.swift
@@ -17,6 +17,8 @@ class RequestDefinitionTests: XCTestCase
         let method: HTTPMethod? = .get
         
         let id: String
+        
+        var optionalFlag: Int? = nil
     }
 }
 
@@ -24,12 +26,23 @@ class RequestDefinitionTests: XCTestCase
 
 extension RequestDefinitionTests
 {
-    func test_buildParameters()
+    func test_buildParameters_withoutOptional()
     {
         let sut = UserRequestDefinition(id: "123").parameters
         
         XCTAssertEqual(sut.count, 1)
         XCTAssertTrue(sut.contains(where: { $0.key == "id" }))
         XCTAssertTrue(sut.contains(where: { ($0.value as? String) == "123" }))
+    }
+    
+    func test_buildParameters_withOptional()
+    {
+        let sut = UserRequestDefinition(id: "123", optionalFlag: 2).parameters
+        
+        XCTAssertEqual(sut.count, 2)
+        XCTAssertTrue(sut.contains(where: { $0.key == "id" }))
+        XCTAssertTrue(sut.contains(where: { ($0.value as? String) == "123" }))
+        XCTAssertTrue(sut.contains(where: { $0.key == "optionalFlag" }))
+        XCTAssertTrue(sut.contains(where: { ($0.value as? Int) == 2 }))
     }
 }
